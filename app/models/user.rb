372de24cef26152dd00848f32c8b5dd0eee6229a
Gofran shukaie <gofranshukair@gmail.com>
2015-05-30 15:04:18 +0100

    Resource rating

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :places
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  letsrate_rater
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
